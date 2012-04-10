d06c2bc32391d8663ed955d8a0c2074e2200e174
Joe Redmon <pjreddie+github@gmail.com>
2012-04-10 15:01:07 -0800

    added htaccess caching

--- kucb/community/handle_upload.py
+++ kucb/community/handle_upload.py
@@ -14,27 +14,27 @@ def ununicode(adata):
     
 
 def handle_uploaded_blotter(f):
-    destination = open('/tmp/blotter.txt', 'wb+')
+    destination = open('blotter.txt', 'wb+')
     for chunk in f.chunks():
         destination.write(chunk)
     destination.close()
-    uploaded = open('/tmp/blotter.txt','rb')
+    uploaded = open('blotter.txt','rb')
+    blots = []
     for line in csv.reader(uploaded):
-        try:
-            line = [ununicode(x) for x in line]
-            if line[0]:
-                line[0] = line[0].strip()
-                line[1] = line[1].strip()
-                line[2] = line[2].strip()
-                line[3] = line[3].strip()
-                datestr = " ".join(line[0:3])
-                date = datetime.datetime.strptime(datestr, "%m/%d/%y %a %H%M")
-                dts = line[3].split("-")
-                kind = dts[0].strip()
-                details = "-".join(dts[1:]).strip()
-                blot = Blot(date = date, kind = kind, details = details)
-                blot.save()
-        except:
-            pass
+        line = [ununicode(x) for x in line]
+        if line[0]:
+            line[0] = line[0].strip()
+            line[1] = line[1].strip()
+            line[2] = line[2].strip()
+            line[3] = line[3].strip()
+            datestr = " ".join(line[0:3])
+            date = datetime.datetime.strptime(datestr, "%m/%d/%y %a %H%M")
+            dts = line[3].split("-")
+            kind = dts[0].strip()
+            details = "-".join(dts[1:]).strip()
+            blot = Blot(date = date, kind = kind, details = details)
+            blots.append(blot)
+    for blot in blots:
+        blot.save()
 
 
