64197a3c1bb9fa01bc5270d387a34490b1fb6821
Unalaska Community Broadcasting <kucb@pine.dreamhost.com>
2012-04-10 17:25:33 -0700

    server settings and stuff

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,7 @@
 import os, sys
-sys.path.append('/Users/jredmon/kucb.org')
-sys.path.append('/Users/jredmon/kucb.org/kucb')
+sys.path.insert(1,"/home/kucb/software/lib/python2.6/site-packages")
+sys.path.append('/home/kucb/kucb.org')
+sys.path.append('/home/kucb/kucb.org/kucb')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'kucb.settings'
 
 import django.core.handlers.wsgi
