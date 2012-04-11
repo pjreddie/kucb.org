6b77484fd5b7f8d09dcd1448436509058817a1cc
Joe Redmon <news@kucb.org>
2012-04-10 22:34:59 -0700

    untracked settings

--- passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-sys.path.insert(1,"/home/kucb/software/lib/python2.6/site-packages")
-sys.path.append('/home/kucb/kucb.org')
-sys.path.append('/home/kucb/kucb.org/kucb')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'kucb.settings'
-
-import django.core.handlers.wsgi
-
-application = django.core.handlers.wsgi.WSGIHandler()
