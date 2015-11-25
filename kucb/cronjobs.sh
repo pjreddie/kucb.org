068a29e5f0e6da6ccc7cadba1caca325f6f8e1ad
Joe Redmon <news@kucb.org>
2015-11-25 11:18:49 -0800

    cronjob path

--- kucb/cronjobs.sh
+++ kucb/cronjobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/kucb/kucb.org/kucb
+cd /home/kucb/archive.kucb.org/kucb
 export PYTHONPATH=$PYTHONPATH:/home/kucb/env/lib/python2.6/site-packages
 export DJANGO_SETTINGS_MODULE=kucb.settings 
 ./manage.py update_feed
